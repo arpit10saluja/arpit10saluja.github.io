400f752a60ecc643d2533bb2eced310be930d347
https://github.com/arpit10saluja/git_we.git <arpitsaluja1234@gmail.com>
2023-08-12 16:16:01 +0530

    projects add

--- src/Components/projects/ProjectsData.js
+++ src/Components/projects/ProjectsData.js
@@ -1,7 +1,9 @@
 export const projectsData = [
   {
-    project_img: "https://user-images.githubusercontent.com/95885406/221484006-d55264fe-6525-47a5-a834-4dd5c8c5531a.png",
-    project_img_dark: "https://user-images.githubusercontent.com/95885406/221484006-d55264fe-6525-47a5-a834-4dd5c8c5531a.png",
+    project_img:
+      "https://user-images.githubusercontent.com/95885406/221484006-d55264fe-6525-47a5-a834-4dd5c8c5531a.png",
+    project_img_dark:
+      "https://user-images.githubusercontent.com/95885406/221484006-d55264fe-6525-47a5-a834-4dd5c8c5531a.png",
 
     title: "Trip To Heaven",
     description:
@@ -36,8 +38,10 @@ export const projectsData = [
     github: "https://github.com/shivammchaudhary1/hesitant-river-6235",
   },
   {
-    project_img: "https://user-images.githubusercontent.com/109855468/234389883-cf1b3f81-0eee-4b0f-a3ee-cd45650e514d.png",
-    project_img_dark: "https://user-images.githubusercontent.com/109855468/234389883-cf1b3f81-0eee-4b0f-a3ee-cd45650e514d.png",
+    project_img:
+      "https://user-images.githubusercontent.com/109855468/234389883-cf1b3f81-0eee-4b0f-a3ee-cd45650e514d.png",
+    project_img_dark:
+      "https://user-images.githubusercontent.com/109855468/234389883-cf1b3f81-0eee-4b0f-a3ee-cd45650e514d.png",
 
     title: "My Shop",
     description:
@@ -70,8 +74,10 @@ export const projectsData = [
     github: "https://github.com/arpit10saluja/casual-metal-9545",
   },
   {
-    project_img: "https://user-images.githubusercontent.com/110247476/229423774-8be01bf0-2bc8-4586-a7bc-785d76bdba85.png",
-    project_img_dark: "https://user-images.githubusercontent.com/110247476/229423774-8be01bf0-2bc8-4586-a7bc-785d76bdba85.png",
+    project_img:
+      "https://user-images.githubusercontent.com/110247476/229423774-8be01bf0-2bc8-4586-a7bc-785d76bdba85.png",
+    project_img_dark:
+      "https://user-images.githubusercontent.com/110247476/229423774-8be01bf0-2bc8-4586-a7bc-785d76bdba85.png",
 
     title: "Petrol",
     description:
@@ -104,8 +110,10 @@ export const projectsData = [
     github: "https://github.com/Sudip-C/hot-caption-4153",
   },
   {
-    project_img: "https://user-images.githubusercontent.com/109855468/233827218-e3d01b64-bd33-4dc7-88f0-7932cd4ab339.png",
-    project_img_dark: "https://user-images.githubusercontent.com/109855468/233827218-e3d01b64-bd33-4dc7-88f0-7932cd4ab339.png",
+    project_img:
+      "https://user-images.githubusercontent.com/109855468/233827218-e3d01b64-bd33-4dc7-88f0-7932cd4ab339.png",
+    project_img_dark:
+      "https://user-images.githubusercontent.com/109855468/233827218-e3d01b64-bd33-4dc7-88f0-7932cd4ab339.png",
     title: "ZoomCar-clone",
     description:
       "This Web-site is a clone of Zoomcar, which provides you to book self drive rently cars. It's an individual project created in the span of 4 days, with tech stack used such as HTML, CSS, JavaScript and Netlify for deployment",
@@ -131,8 +139,10 @@ export const projectsData = [
     github: "https://github.com/arpit10saluja/accidental-discussion-3173",
   },
   {
-    project_img: "https://user-images.githubusercontent.com/109855468/230036445-a84b08dc-fcb6-460f-af51-b4d07ee6faed.png",
-    project_img_dark: "https://user-images.githubusercontent.com/109855468/230036445-a84b08dc-fcb6-460f-af51-b4d07ee6faed.png",
+    project_img:
+      "https://user-images.githubusercontent.com/109855468/230036445-a84b08dc-fcb6-460f-af51-b4d07ee6faed.png",
+    project_img_dark:
+      "https://user-images.githubusercontent.com/109855468/230036445-a84b08dc-fcb6-460f-af51-b4d07ee6faed.png",
     title: "Fit-me:- The Running Club",
     description:
       "This project is a clone of CultFit, it's a fitness website where user can join the fitness enviourment and track there fitness that help them to achieve there fitness goal. This project created by team of 5 starting with Pujarini Sahoo(team lead), Arpit Kumar, Dheeraj Prajapati, Yash Srivastava and Manjunath Naik, in span of 5 days.",
@@ -158,8 +168,10 @@ export const projectsData = [
     github: "https://github.com/puja2795/shut-point-1924",
   },
   {
-    project_img: "https://user-images.githubusercontent.com/109855468/234400705-b544c348-c569-4eef-9a59-16035b141e46.png",
-    project_img_dark: "https://user-images.githubusercontent.com/109855468/234400705-b544c348-c569-4eef-9a59-16035b141e46.png",
+    project_img:
+      "https://user-images.githubusercontent.com/109855468/234400705-b544c348-c569-4eef-9a59-16035b141e46.png",
+    project_img_dark:
+      "https://user-images.githubusercontent.com/109855468/234400705-b544c348-c569-4eef-9a59-16035b141e46.png",
     title: "Youtube-clone",
     description:
       "This web-site is a clone of Youtube, where you can sort the list of videos by views, alphabet and popularity and you can also search from the navbar. with tech stack such as HTML, CSS, JavaScript and Netlify for deployment",
@@ -185,9 +197,60 @@ export const projectsData = [
     github: "https://github.com/arpit10saluja/YouTube-clone",
   },
   {
-    project_img: "",
-    project_img_dark: "",
-    title: "",
+    project_img:
+      "https://user-images.githubusercontent.com/104666876/238284764-f9e1c656-3e96-465d-8ac6-ca1b9ca307d3.png",
+    project_img_dark:
+      "https://user-images.githubusercontent.com/104666876/238284764-f9e1c656-3e96-465d-8ac6-ca1b9ca307d3.png",
+    title: "FitFusion",
+    description:
+      "FitFusion is a fitness dashboard web application that provides users with a comprehensive platform to track and manage their fitness goals. Whether you're an athlete, fitness enthusiast, or just getting started on your wellness journey, FitFusion offers a range of features to help you stay motivated and achieve your fitness targets. This project is built using the MERN (MongoDB, Express.js, React.js, Node.js) stack, delivering a seamless and engaging user experience.",
+    tech_stack: [
+      {
+        url: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+        name: "html",
+      },
+      {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
+        name: "css",
+      },
+      {
+        url: "https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg",
+        name: "js",
+      },
+      {
+        url: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+        name: "github",
+      },
+      {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+        name: "react",
+      },
+      {
+        url: "https://images.opencollective.com/chakra-ui-pro/61bd1dd/logo/256.png",
+        name: "chakra",
+      },
+      {
+        url: "https://img.icons8.com/fluency/240/node-js.png",
+        name: "node",
+      },
+      {
+        url: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/100/external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo.png",
+        name: "mongoDB",
+      },
+      {
+        url: "https://img.icons8.com/color/256/express-js.png",
+        name: "express-js",
+      },
+    ],
+    link: "https://fitfusion.vercel.app/",
+    github: "https://github.com/arpit10saluja/fitfusion",
+  },
+  {
+    project_img:
+      "",
+    project_img_dark:
+      "",
+    title: "Code Converter",
     description:
       "",
     tech_stack: [
@@ -212,9 +275,57 @@ export const projectsData = [
     github: "",
   },
   {
-    project_img: "",
-    project_img_dark: "",
-    title: "",
+    project_img: "https://user-images.githubusercontent.com/104666876/238126388-239de8ab-8642-4058-a53e-31bf43f08624.png",
+    project_img_dark: "https://user-images.githubusercontent.com/104666876/238126388-239de8ab-8642-4058-a53e-31bf43f08624.png",
+    title: "Clotho",
+    description: "Clotho is a MERN stack e-commerce clone of Koovs. It encompasses a homepage, product listings, cart functionality, and an admin dashboard with product management and order tracking. Utilizing Redux for state management and MongoDB as the database, Clotho replicates a seamless shopping experience while offering efficient backend administration.",
+    tech_stack: [
+      {
+        url: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+        name: "html",
+      },
+      {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
+        name: "css",
+      },
+      {
+        url: "https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg",
+        name: "js",
+      },
+      {
+        url: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+        name: "github",
+      },
+      {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+        name: "react",
+      },
+      {
+        url: "https://images.opencollective.com/chakra-ui-pro/61bd1dd/logo/256.png",
+        name: "chakra",
+      },
+      {
+        url: "https://img.icons8.com/fluency/240/node-js.png",
+        name: "node",
+      },
+      {
+        url: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/100/external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo.png",
+        name: "mongoDB",
+      },
+      {
+        url: "https://img.icons8.com/color/256/express-js.png",
+        name: "express-js",
+      },
+    ],
+    link: "",
+    github: "",
+  },
+  {
+    project_img:
+      "",
+    project_img_dark:
+      "",
+    title: "Joke Generator",
     description:
       "",
     tech_stack: [
@@ -237,5 +348,5 @@ export const projectsData = [
     ],
     link: "",
     github: "",
-  }
+  },
 ];
